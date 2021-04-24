4e0a25e619a3383ec2a3cec286f7100173ae6845
mickfree <71110727@certus.edu.pe>
2021-04-23 20:21:10 -0500

    Create Ejercicio_2.py

---
+++ Ejercicio_2.py
@@ -0,0 +1,10 @@
+a = 14
+b = 12
+c = a + b
+print(a,"hola", b, "=",c)
+
+#print(a+"hola"+ b+ "="+c)
+
+#A la hora de concatenar números y cadena de texto, es necesario convertir las variables en texto:
+
+print(str(a)+" hola "+ str(b)+ " = "+str(c ))
