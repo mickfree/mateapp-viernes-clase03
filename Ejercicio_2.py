f2b17179fe08ba5d00a51d23b00550d282af3063
mickfree <71110727@certus.edu.pe>
2021-04-23 20:54:59 -0500

    Update Ejercicio_2.py

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,10 +1,19 @@
 a = 14
 b = 12
-c = a + b
+c = str(a + b)
+#print(c)
+
 print(a,"hola", b, "=",c)
 
 #print(a+"hola"+ b+ "="+c)
 
 #A la hora de concatenar números y cadena de texto, es necesario convertir las variables en texto:
+hola = "hola"
+igual = "="
 
-print(str(a)+" hola "+ str(b)+ " = "+str(c ))
+print(str(a)+" hola "+ str(b)+ " = " + str(c))
+
+print(str(a)+" hola "+ str(b)+ " = " +c)
+
+print(str(a)+ hola+ str(b)+ igual +c)
+
