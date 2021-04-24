7dfbc7fb179cd3c5e51254a7824cae0da72421f0
mickfree <71110727@certus.edu.pe>
2021-04-23 22:07:18 -0500

    Create Ejercicio_3.py

---
+++ Ejercicio_3.py
@@ -0,0 +1,32 @@
+s = "Michael Luna"
+print(s.lower())
+print(s.upper())
+print(s.count("a"))
+
+a =  20
+print(a)
+a = a+1
+print(a)
+a+=1
+print(a)
+a*=2
+print(a)
+a/=2
+print(a)
+a-=2
+print(a)
+a**=2
+print(a)
+
+
+j = 3 + 2
+if j ==5:
+    print("J es igual a 5")
+else:
+    print("J no es igaul a 5")
+
+numeros = [1, 2, 3]
+
+for n in numeros:
+    print(n)
+
